be36a9941302713a0449cca3579e7aaa04260b00
Savannah Israel <sisrael0405@gmail.com>
2020-02-20 12:23:17 -0700

    basic file structure

---
+++ wordGuessGame/assets/javascript/game.js
@@ -0,0 +1,20 @@
+// Variables
+    // word bank
+
+    // current word
+
+    // wins
+
+    // losses
+
+    // guesses
+
+    // guesses remaining
+
+    // letters guessed
+
+
+
+// function to select random word from word bank
+
+// function to get selected word length and display blank spaces
